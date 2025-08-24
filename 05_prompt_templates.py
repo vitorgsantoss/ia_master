7d66b8273fdf12e11fa338ca6dfa088b7ee2a567
Vitor Santos <vg452004@gmail.com>
2025-08-23 21:14:34 -0300

    Aprendendo sobre prompt templates

---
+++ 05_prompt_templates.py
@@ -0,0 +1,26 @@
+from langchain_openai import ChatOpenAI
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+template = '''
+Traduza o texto de {idioma1} para {idioma2}: {texto}
+'''
+
+prompt_template = PromptTemplate(template=template)
+
+prompt = prompt_template.format(
+    idioma1 = 'inglês', 
+    idioma2 = 'português',
+    texto = 'How old are you?'
+)
+
+model = ChatOpenAI(
+    model = 'gpt-3.5-turbo'
+)
+
+response = model.invoke(prompt)
+
+print(response.content)
+
